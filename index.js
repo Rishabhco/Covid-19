0f943c1c7f57ed181025f456069b3d07ccdf5bd0
Rishabhco <83543522+Rishabhco@users.noreply.github.com>
2021-07-20 01:10:52 +0530

    modified__commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function shw_alert(){
     return alert("Please enter your name !!!");
     else if(e==="" && mn==="")
     return alert("Please give some contact informtion");
+    else if(e==="")
+    return alert("Your response has been submitted !!\n"+"Name: "+n+"\nEmail: Not Given "+"\nPhone Number: "+mn);
+    else if(mn==="")
+    return alert("Your response has been submitted !!\n"+"Name: "+n+"\nEmail: "+e+"\nPhone Number: Not Given");
     else
     return alert("Your response has been submitted !!\n"+"Name: "+n+"\nEmail: "+e+"\nPhone Number: "+mn);
 
